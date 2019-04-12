d0e5896c605ddd9407121eafe435f1745e8fd67a
unoctium1 <unoctium1@gmail.com>
2019-04-11 19:24:09 -0700

    Added new data structures

--- Structs.cs
+++ Structs.cs
@@ -16,22 +16,19 @@ namespace ARDesign
             public string Db;
             // List of widgets in the room
             public IList<DBWidget> Widgets;
+
+            public string Building;
+            public string Room;
         }
 
         /// <summary>
         /// Struct for storing widget info - Designed for JSON deserialization
-        /// \todo Move room config out of struct
-        /// \todo Add value tags to struct
         /// </summary>
         public struct DBWidget
         {
-
             public Vector3 Position;
             public string Measure;
-
-            // Possibly move these to DBScene?
-            public string Building;
-            public string Room;
+            public string[] values;
         }
 
     }
