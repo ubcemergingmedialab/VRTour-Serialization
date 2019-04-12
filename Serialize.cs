7028dcfc83abea0524842154a36f47a3633a6703
unoctium1 <unoctium1@gmail.com>
2019-04-11 19:39:39 -0700

    Added namespace comments

---
+++ Serialize.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ARDesign
+{
+    /// <summary>
+    /// Pipeline for passing scene configuration info. 
+    /// Essentially involves either building a DBScene or deserializing it. 
+    /// In use in both ARDesign project and ARDesign setup project, as a submodule
+    /// </summary>
+    namespace Serialize { };
+}
+
